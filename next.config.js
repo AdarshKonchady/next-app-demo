56d3fc5e46a8cec3b5083bcdbc49f66a9332a097
Adarsh Konchady <akonchady@gmail.com>
2022-07-07 15:04:49 -0500

    prettier

--- next.config.js
+++ next.config.js
@@ -3,11 +3,11 @@ const nextConfig = {
   experimental: {
     swcPlugins: [
       [
-        '/Users/Z001ZLM/Documents/Work/swc-console-replace-plugin/target/wasm32-wasi/release/console_replace.wasm',
+        "/Users/Z001ZLM/Documents/Work/swc-console-replace-plugin/target/wasm32-wasi/release/console_replace.wasm",
       ],
     ],
   },
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

edabe70088c54e5003f95f8203a7742de03b87f3
Adarsh Konchady <adarsh.konchady@target.com>
2022-07-07 15:03:56 -0500

    plugin works !

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    swcPlugins: [
+      [
+        '/Users/Z001ZLM/Documents/Work/swc-console-replace-plugin/target/wasm32-wasi/release/console_replace.wasm',
+      ],
+    ],
+  },
   reactStrictMode: true,
 }
 

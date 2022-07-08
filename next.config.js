da8cd36b41a79236016686039380dc9421f18d13
Adarsh Konchady <akonchady@gmail.com>
2022-07-07 21:52:16 -0500

    empty object per comment

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
     swcPlugins: [
       [
         "/Users/Z001ZLM/Documents/Work/swc-console-replace-plugin/target/wasm32-wasi/release/console_replace.wasm",
+        {},
       ],
     ],
   },
